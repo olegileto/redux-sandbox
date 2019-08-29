b42a8bae0ac9bb3ca574b3c24b86346f38f3d597
Oleg Khmelevskoy <olegileto@gmail.com>
2019-08-29 14:44:37 +0300

    Connect(), mapStateToProps, mapDispatchToProps

--- src/index.js
+++ src/index.js
@@ -1,34 +1,16 @@
-import {createStore, bindActionCreators} from "redux";
+import React from 'react';
+import ReacDOM from 'react-dom';
+import {createStore} from "redux";
+import {Provider} from 'react-redux';
 
-import * as actions from './actions';
 import {reducer} from "./reducer";
 
+import App from "./components/App";
 
 const store = createStore(reducer);
-const {dispatch} = store;
 
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-
-document
-    .getElementById('inc')
-    .addEventListener('click', inc);
-
-document
-    .getElementById('dec')
-    .addEventListener('click', dec);
-
-document
-    .getElementById('rnd')
-    .addEventListener('click', () => {
-        const payload = Math.floor(Math.random() * 10);
-        rnd(payload)
-    });
-
-
-const update = () => {
-    document
-        .getElementById('counter')
-        .innerHTML = store.getState();
-};
-
-store.subscribe(update);
+ReacDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
